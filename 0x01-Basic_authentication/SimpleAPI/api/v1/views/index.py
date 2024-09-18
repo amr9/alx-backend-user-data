2e86938a1b3356e583cfeaefd58349862a7c6695
amr9 <amr12201148@yahoo.com>
2024-09-19 02:07:08 +0300

    app index users

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """endpoint that raise a 401 error"""
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def Forbidden() -> str:
+    """endpoint that raise a 403 error"""
+    abort(403)
